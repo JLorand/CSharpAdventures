01022cacdf65f81c76cf6a68f61b713b46c7405b
Ferenc Lorand Jakab <fjakab@progesoftware.it>
2019-06-28 23:57:04 +0200

    Add project files.

---
+++ Generics/Messenger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    public class Messenger<T>
+    {
+        private readonly T _message;
+
+        public Messenger(T message)
+        {
+            _message = message;
+        }
+
+        public GenericResult<T> GetBaseResponse()
+        {
+            return new GenericResult<T>()
+            {
+                Success = true,
+                Data = _message,
+                Type = _message.GetType()
+            };
+        }
+
+        public GenericResult<T> GetNewMessage(T message)
+        {
+            return new GenericResult<T>()
+            {
+                Success = true,
+                Data = message,
+                Type = message.GetType()
+            };
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Messenger<string> stringMessenger = new Messenger<string>("Base message");
+            var result1 = stringMessenger.GetBaseResponse();
+            Console.WriteLine($"First result:{Environment.NewLine}" +
+                $"\tSuccess: {result1.Success}{Environment.NewLine}" +
+                $"\tData: {result1.Data}{Environment.NewLine}" +
+                $"\tData type: {result1.Type}");
+
+            Messenger<int> intMessenger = new Messenger<int>(1);
+            var result2 = intMessenger.GetBaseResponse();
+            Console.WriteLine($"Second result:{Environment.NewLine}" +
+                $"\tSuccess: {result2.Success}{Environment.NewLine}" +
+                $"\tData: {result2.Data}{Environment.NewLine}" +
+                $"\tData type: {result2.Type}");
+
+            Messenger<bool> boolMessenger = new Messenger<bool>(true);
+            var result3 = boolMessenger.GetBaseResponse();
+            Console.WriteLine($"Third result:{Environment.NewLine}" +
+                $"\tSuccess: {result3.Success}{Environment.NewLine}" +
+                $"\tData: {result3.Data}{Environment.NewLine}" +
+                $"\tData type: {result3.Type}");
+
+            Messenger<string> newMessenger = new Messenger<string>("BaseMessage");
+            var result4 = newMessenger.GetNewMessage("New message");
+            Console.WriteLine($"Fourth result:{Environment.NewLine}" +
+                $"\tSuccess: {result4.Success}{Environment.NewLine}" +
+                $"\tData: {result4.Data}{Environment.NewLine}" +
+                $"\tData type: {result4.Type}");
+        }
+    }
+}
